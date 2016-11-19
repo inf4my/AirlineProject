429367846fb6b5df7dee7c2e97d0d3eecefd6ec7
kevinlionery <kevinlionery@gmail.com>
2016-11-19 21:09:53 +0700

    Commit Kevin

--- src/Controller/Main.java
+++ src/Controller/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("menuUtama.fxml"));
-        primaryStage.setTitle("Airline");
+        primaryStage.setTitle("Airlines");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
