28bcc8d95830048431a043c0a867004607948e59
Aldo Febrian <aldo_zou96@yahoo.com>
2016-11-24 11:38:20 +0700

    + exception, Customer class

--- src/BusinessLayer/Customer.java
+++ src/BusinessLayer/Customer.java
@@ -1,13 +1,16 @@
 package BusinessLayer;
 
+import javafx.scene.control.Alert;
+
 import javax.xml.bind.DatatypeConverter;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 
+import Exception.*;
 /**
  * Created by GDC on 11/24/16.
  */
-public class Customer {
+public class Customer{
     private String id;
     private String fname;
     private String lname;
@@ -27,9 +30,27 @@ public class Customer {
         }
     }
 
-    public Customer(String fname, String lname, String id, String username, String password){
-        if(fname.equals("")){
+    public Customer(String fname, String lname, String id, String username, String password)throws EmptyPropertyException{
+        if(fname.equals("")) throw new EmptyPropertyException("Firstname field is required");
+        if(lname.equals("")) throw new EmptyPropertyException("Lastname field is required");
+        if(id.equals("")) throw new EmptyPropertyException("ID field is required");
+        if(username.equals("")) throw new EmptyPropertyException("Username field is required");
+        if(password.equals("")) throw new EmptyPropertyException("Password field is required");
 
+        this.fname=fname;
+        this.lname=lname;
+        this.id=id;
+        this.username=username;
+        this.password=hash(password);
+    }
+
+    public boolean login(String username, String password){
+        if(this.username.equals(username)){
+            if(this.password.equals(hash(password))){
+                return true;
+            }
+            return false;
         }
+        return false;
     }
 }
