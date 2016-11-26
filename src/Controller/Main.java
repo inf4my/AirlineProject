c34ae789cddf3197e50f6a3d199e2c21b1bf2e94
kevinlionery <kevinlionery@gmail.com>
2016-11-26 20:56:10 +0700

    menuUtama updated_26nov_3

--- src/Controller/Main.java
+++ src/Controller/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -13,11 +14,9 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("menuUtama.fxml"));
         primaryStage.setTitle("Airlines");
         primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.getIcons().add(new Image(this.getClass().getResource("airplane.png").toString()));
         primaryStage.show();
     }
-
-
-
 
     public static void main(String[] args) {
         launch(args);
