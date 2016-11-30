90fbb44ed02fdab97719a971c6a6518b77ae830c
inf4my <naufal.irfan@yahoo.com>
2016-11-30 15:28:53 +0700

    Push signUp.fxml, nambah class customer al, nyesuain class customer dengan DB

---
+++ src/Controller/ControllerSignUp.java
@@ -0,0 +1,27 @@
+package Controller;
+
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import BusinessLayer.Customer;
+import Exception.EmptyPropertyException;
+import DataLinkLayer.ConnectionConfiguration;
+
+/**
+ * Created by inf4my on 11/30/2016.
+ */
+public class ControllerSignUp {
+    Customer user;
+    @FXML
+    JFXTextField txtNamaLengkap, txtNamaAkun;
+
+    @FXML
+    JFXPasswordField txtPassword, txtPasswordUlang;
+
+    @FXML
+    public void handleBtnDaftar() throws EmptyPropertyException {
+        user = new Customer(txtNamaLengkap.getText(), txtNamaAkun.getText(), txtPassword.getText());
+
+    }
+}
--- src/DataLinkLayer/ConnectionConfiguration.java
+++ src/DataLinkLayer/ConnectionConfiguration.java
@@ -7,7 +7,7 @@ import java.sql.DriverManager;
  * Created by Naufal on 11/18/2016.
  */
 public class ConnectionConfiguration {
-    private static final String connectionString = "jdbc:mysql://localhost:3306/dummy";
+    private static final String connectionString = "jdbc:mysql://localhost:3306/";
     private static final String username = "root";
     private static final String password = "";
 
