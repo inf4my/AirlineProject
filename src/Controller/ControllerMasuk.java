4203c830832ca20df5a9d17b19796e273e03902a
kevinlionery <kevinlionery@gmail.com>
2016-11-30 15:19:26 +0700

    update_masuk_dan_kalender

--- src/Controller/ControllerMasuk.java
+++ src/Controller/ControllerMasuk.java
@@ -1,7 +1,13 @@
 package Controller;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
 import javafx.fxml.FXML;
+
+import javax.xml.bind.DatatypeConverter;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
 
 /**
  * Created by lionery on 30/11/2016.
@@ -11,7 +17,20 @@ public class ControllerMasuk {
     JFXButton btnBatal;
 
     @FXML
+    JFXTextField txtNamaakun;
+
+    @FXML
+    JFXPasswordField passAkun;
+
     public void handleBtnBatal(){
         btnBatal.getScene().getWindow().hide();
     }
-}
+
+    public void handleBtnMasuk(){
+        String username = txtNamaakun.getText();
+        String password = passAkun.getText();
+
+    }
+
+
+    }
