3d44e9b938d9ecfe9e0df06b126a7822764eb13e
Aldo Febrian <aldo_zou96@yahoo.com>
2016-12-13 22:14:52 +0700

    metodeBayar

---
+++ src/Controller/MetodeBayarController.java
@@ -0,0 +1,26 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+
+/**
+ * Created by aldofebrian on 13/12/2016.
+ */
+public class MetodeBayarController {
+    public void handleBtnBca(ActionEvent actionEvent) {
+    }
+
+    public void handleBtnKlik(ActionEvent actionEvent) {
+    }
+
+    public void handleBtnMandiri(ActionEvent actionEvent) {
+    }
+
+    public void handleBtnMaybank(ActionEvent actionEvent) {
+    }
+
+    public void handleBtnCimb(ActionEvent actionEvent) {
+    }
+
+    public void handleBtnVisa(ActionEvent actionEvent) {
+    }
+}
