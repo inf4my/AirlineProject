02abe5c660c3cd179feffe6fefd1b66718a9d82d
Naufal <naufal.irfan@yahoo.com>
2016-12-14 05:46:07 +0700

    Confirmation sama metodebayar

--- src/Controller/ControllerConfirmation.java
+++ src/Controller/ControllerConfirmation.java
@@ -1,5 +1,7 @@
 package Controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.sun.corba.se.spi.orbutil.fsm.Action;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -19,6 +21,9 @@ public class ControllerConfirmation {
     @FXML
     TextField txtKodePenerbangan1, txtBerangkat1, txtTujuan1, txtWaktuBerangkat1, txtWaktuSampai1, txtKelas1, txtTotalBiaya1;
 
+    @FXML
+    JFXButton btnExit;
+
     public void initialize(String kode, String berangkat, String tujuan, String waktuBerangkat, String waktuSampai, String kelas, String totalBiaya){
         txtKodePenerbangan.setText(kode);
         txtBerangkat.setText(berangkat);
@@ -31,13 +36,21 @@ public class ControllerConfirmation {
 
     public void initialize(String kode, String berangkat, String tujuan, String waktuBerangkat, String waktuSampai, String kelas, String totalBiaya, String kodePenerbangan2
     , String berangkat2, String tujuan2, String waktuBerangkat2, String waktuSampai2, String totalBiaya2){
-        txtKodePenerbangan.setText(kode);
+                txtKodePenerbangan.setText(kode);
         txtBerangkat.setText(berangkat);
         txtTujuan.setText(tujuan);
         txtWaktuBerangkat.setText(waktuBerangkat);
         txtWaktuSampai.setText(waktuSampai);
         txtKelas.setText(kelas);
         txtTotalBiaya.setText(totalBiaya);
+
+        txtKodePenerbangan.setEditable(false);
+        txtBerangkat.setEditable(false);
+        txtTujuan.setEditable(false);
+        txtWaktuBerangkat.setEditable(false);
+        txtWaktuSampai.setEditable(false);
+        txtKelas.setEditable(false);
+        txtTotalBiaya.setEditable(false);
 
         txtKodePenerbangan1.setText(kodePenerbangan2);
         txtBerangkat1.setText(berangkat2);
@@ -54,7 +67,7 @@ public class ControllerConfirmation {
             Parent entryForm = loader.load();
             Stage entryStage = new Stage();
             entryStage.setTitle("Searching Flight");
-            entryStage.setScene(new Scene(entryForm, 1200,720));
+            entryStage.setScene(new Scene(entryForm, 735,409));
             entryStage.show();
             entryStage.requestFocus();
             entryStage.getIcons().add(new Image(this.getClass().getResource("search.png").toString()));
@@ -65,5 +78,10 @@ public class ControllerConfirmation {
         catch (Exception e){
             e.printStackTrace();
         }
+    }
+
+    @FXML
+    public void handleBtnExit(ActionEvent event){
+        btnExit.getScene().getWindow().hide();
     }
 }
--- src/Controller/MetodeBayarController.java
+++ src/Controller/MetodeBayarController.java
@@ -1,26 +1,59 @@
 package Controller;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.image.ImageView;
 
 /**
  * Created by aldofebrian on 13/12/2016.
  */
 public class MetodeBayarController {
     public void handleBtnBca(ActionEvent actionEvent) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Dialog Informasi");
+        alert.setHeaderText(null);
+        alert.setContentText("Silahkan lakukan pembayaran di bank yang anda pilih");
+
+        alert.setGraphic(new ImageView(this.getClass().getResource("bca.png").toString()));
     }
 
     public void handleBtnKlik(ActionEvent actionEvent) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Dialog Informasi");
+        alert.setHeaderText(null);
+        alert.setContentText("Silahkan lakukan pembayaran di bank yang anda pilih");
+        alert.setGraphic(new ImageView(this.getClass().getResource("login.png").toString()));
     }
 
     public void handleBtnMandiri(ActionEvent actionEvent) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Dialog Informasi");
+        alert.setHeaderText(null);
+        alert.setContentText("Silahkan lakukan pembayaran di bank yang anda pilih");
+        alert.setGraphic(new ImageView(this.getClass().getResource("login.png").toString()));
     }
 
     public void handleBtnMaybank(ActionEvent actionEvent) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Dialog Informasi");
+        alert.setHeaderText(null);
+        alert.setContentText("Silahkan lakukan pembayaran di bank yang anda pilih");
+        alert.setGraphic(new ImageView(this.getClass().getResource("login.png").toString()));
     }
 
     public void handleBtnCimb(ActionEvent actionEvent) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Dialog Informasi");
+        alert.setHeaderText(null);
+        alert.setContentText("Silahkan lakukan pembayaran di bank yang anda pilih");
+        alert.setGraphic(new ImageView(this.getClass().getResource("login.png").toString()));
     }
 
     public void handleBtnVisa(ActionEvent actionEvent) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Dialog Informasi");
+        alert.setHeaderText(null);
+        alert.setContentText("Silahkan lakukan pembayaran di bank yang anda pilih");
+        alert.setGraphic(new ImageView(this.getClass().getResource("login.png").toString()));
     }
 }
